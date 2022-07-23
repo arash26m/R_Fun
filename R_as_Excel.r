a18d0ca6d5a12f35722e7a185e7dbaf48034d4b1
Arash Taghinezhad <54901882+arash26m@users.noreply.github.com>
2022-07-23 12:35:39 -0700

    Update R_as_Excel.r

--- R_as_Excel.r
+++ R_as_Excel.r
@@ -1,5 +1,30 @@
-#column frequency
-# creating a data frame
+#--------------------------------R for data science
+#https://r4ds.had.co.nz/index.html
+#--------------------------------dplyr is a package for working with data like Excel
+#https://dplyr.tidyverse.org/
+library(dplyr)
+arrange(mtcars, cyl, am)
+mtcars %>% arrange(cyl, am)
+mtcars %>% arrange(desc(cyl), am)
+select(mtcars, mpg, cyl)
+mtcars %>% select(mpg, cyl)
+mtcars %>% select(mpg, cyl) %>% arrange(mpg)
+#
+x=1:10
+summary(x)
+x %>% summary ()
+#
+trees
+summary(select(trees, Height, Volume))
+#
+trees %>%
+  select(Height, Volume) %>%
+  filter(Height>80) %>%
+  summary()
+#
+
+#--------------------------------column frequency
+#creating a data frame
 df <- data.frame(col1 =  sample(letters[1:3], 8, 
                                 replace = TRUE) ,
                  col2 = sample(letters[1:3], 8,
@@ -8,10 +33,10 @@ df <- data.frame(col1 =  sample(letters[1:3], 8,
                                replace = TRUE),
                  col4 = sample(letters[1:3], 8, 
                                replace = TRUE))
-
+#unique values
 aa<- unique(unlist(df))
-
 # apply the summation per value 
 freq <- sapply(df, function(x) table(factor(x, levels = aa, ordered = TRUE)))
 print ("Count of variables per column")
 print (freq)
+#--------------------------------
